ce1ec037140c3da2f05b4e7ffa0dcfb65b6a01f2
rasmussen-brandon <brasmussen@nevada.unr.edu>
2018-01-29 14:09:29 -0800

    cleanup

--- coarsen.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import imageio as ii
-import os
-from scipy import misc
-
-for imagename in os.listdir(os.getcwd() + '/HR'):
-    hrimage = ii.imread(os.getcwd() + '/HR/' + imagename)
-    lrimage = misc.imresize(hrimage, size=(135, 135), interp='bicubic')
-    misc.imsave(os.getcwd() + '/LR/LR_' + imagename, lrimage)
--- make_vals.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-from shutil import copyfile
-
-for imagename in os.listdir(os.getcwd() + '/HR'):
-    chance = np.random.rand()
-    if chance > .85:
-        copyfile(os.getcwd() + '/HR/' + imagename, os.getcwd() + '/HRV/' + imagename)
-        copyfile(os.getcwd() + '/LR/LR_' + imagename, os.getcwd() + '/LRV/' + imagename)
