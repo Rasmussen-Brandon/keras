01269a7f32e7079225d3be3a97a93eaac5f0dfb6
Ubuntu <csp_truckee@ds-hatchery-truckee.hcswdqkkyknelj5csa2nsu0szf.xx.internal.cloudapp.net>
2018-01-30 22:34:49 +0000

    modded_for_azure

--- run_cnn_image.sh
+++ run_cnn_image.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo nvidia-docker run -v $(pwd):/notebooks -it -p 8888:8888 --rm brandonrasmussen/cnn_gpu:latest 
-sudo pip install --upgrade keras
+sudo nvidia-docker run -v $(pwd):/notebooks -it -p 8080:8888 --rm cspinc/big_docker:latest
 
